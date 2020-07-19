0c84093fb9a271d9498fe38a7abe020c0190e541
Kiran Sanganee <kirans@kirans-mac.home>
2020-07-19 14:05:28 +0100

    removed a region

--- routes/url.js
+++ routes/url.js
@@ -5,7 +5,7 @@ const AWS = require('aws-sdk');
 AWS.config.update({
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_ACCESS_KEY_ID,
-  region: "eu-west-2",
+  // region: "eu-west-2",
   // endpoint: "http://localhost:8000"
 });
 
